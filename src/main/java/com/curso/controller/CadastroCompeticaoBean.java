0185eed3f5a90a3c81a1bb899cb060eaf2d3e3c0
alvescode <videoliva@gmail.com>
2024-10-21 12:30:13 -0300

    adiciona cadastro e pesquisa de competicao

---
+++ src/main/java/com/curso/controller/CadastroCompeticaoBean.java
@@ -0,0 +1,50 @@
+package com.curso.controller;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.inject.Inject;
+import com.curso.util.MessageUtil;
+import com.curso.modelo.Atleta;
+import com.curso.modelo.Competicao;
+import com.curso.util.NegocioException;
+import com.curso.service.CompeticaoService;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class CadastroCompeticaoBean implements Serializable {
+    private static final long serialVersionUID = 1L; // necessário por causa do Serializable
+
+    private Competicao competicao;
+
+    @Inject
+    private CompeticaoService competicaoService;
+
+    @PostConstruct
+    public void inicializar() {
+        limpar();
+    }
+
+    public void limpar() {
+        competicao = new Competicao();
+    }
+
+    public void salvar() {
+        try {
+            competicaoService.salvar(competicao);
+            MessageUtil.sucesso("Competição registrada! Sucesso.");
+            limpar();
+        } catch (NegocioException e) {
+            MessageUtil.erro(e.getMessage());
+        }
+    }
+
+    public Competicao getCompeticao() {
+        return competicao;
+    }
+
+    public void setCompeticao(Competicao competicao) {
+        this.competicao = competicao;
+    }
+}
---
+++ src/main/java/com/curso/dao/CompeticaoDAO.java
@@ -0,0 +1,43 @@
+package com.curso.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+import com.curso.modelo.Competicao;
+import com.curso.util.NegocioException;
+import com.curso.util.jpa.Transactional;
+
+public class CompeticaoDAO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private EntityManager manager;
+
+    @Transactional
+    public void salvar(Competicao competicao) {
+        manager.merge(competicao);// altera o que mudou na entidade
+    }
+
+    @Transactional
+    public void excluir(Competicao competicao) throws NegocioException {
+        competicao = buscarPeloCodigo(competicao.getCodigo());
+        try {
+            manager.remove(competicao);
+            manager.flush();
+        } catch (PersistenceException e) {
+            throw new NegocioException("Esta competição não pode ser excluída.");
+        }
+    }
+
+    public Competicao buscarPeloCodigo(Long codigo) {
+        return manager.find(Competicao.class, codigo);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Competicao> buscarTodos() {
+        return manager.createNamedQuery("Competicao.buscarTodos").getResultList();
+    }
+}
